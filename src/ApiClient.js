f6c9ea7eb54ec85c5480c533aa3860dcf0968d88
Osama Salama <osama.salama@wire.com>
2025-11-20 15:15:22 +0100

    chore(metadata): Updating sdk with metadata changes

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -48,7 +48,7 @@ class ApiClient {
         this.authentications = {
         }
 
-        /**
+	/**
          * The default HTTP headers to be included for all API calls.
          * @type {Array.<String>}
          * @default {}
@@ -72,7 +72,7 @@ class ApiClient {
          */
         this.cache = true;
 
-        /**
+	/**
          * If set to true, the client will save the cookies from each server
          * response, and return them in the next request.
          * @default false
